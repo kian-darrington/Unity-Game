f1cc92001d94799672f4a7cce9347da2dc6231d2
Ericpace644 <pacee@rsd407.org>
2022-11-10 08:23:56 -0800

    Revert "Merge branch 'Grabber' into Eric-Branch"
    This reverts commit 658a64fd6ea34605db1636d009dec241457132fd, reversing
    changes made to 7326a7b2474e47763b4acabb0b91e0354d5a0d97.

--- TBKR/Assets/Scripts/Equipment System/Item.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Item", menuName = "Inentory/Item")]
-public class Item : ScriptableObject
-{
-    new public string name = "New Item";
-    public Sprite icon = null;
-}
