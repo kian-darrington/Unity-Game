4169781898ca596fc249563558377da30cf8054e
Ericpace644 <pacee@rsd407.org>
2022-12-14 09:25:31 -0800

    Ability to change scenes

--- TBKR/Assets/Scripts/MenuControl.cs
+++ TBKR/Assets/Scripts/MenuControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuControl : MonoBehaviour
 {
@@ -29,7 +30,7 @@ public class MenuControl : MonoBehaviour
 
     public void NewGame()
     {
-
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void Credits()
