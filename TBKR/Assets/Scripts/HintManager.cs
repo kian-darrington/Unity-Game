16ae6311b1a6219add4f07d81d800c7d845eaf2f
Ericpace644 <pacee@rsd407.org>
2023-01-13 08:56:55 -0800

    b ghfd ghjczdfcghuef

---
+++ TBKR/Assets/Scripts/HintManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintManager : MonoBehaviour
+{
+    public BoxCollider2D Trigger1;
+
+    public BoxCollider2D Trigger2;
+
+    public GameObject Text1;
+
+    public GameObject Text2;
+
+    public GameObject Body;
+
+    public void Hint1()
+    {
+        if (Trigger1 )
+        {
+
+        }
+    }
+
+    public void Hint2()
+    {
+
+    }
+}
