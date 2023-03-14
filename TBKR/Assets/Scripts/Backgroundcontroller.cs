b92de03363707ac208c218b7fdb397f77acc3014
Ericpace644 <90404067+Ericpace644@users.noreply.github.com>
2023-03-14 10:54:51 -0700

    nejkrh

---
+++ TBKR/Assets/Scripts/Backgroundcontroller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backgroundcontroller : MonoBehaviour
+{
+
+	public GameObject Background;
+	public GameObject Foreground;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        
+    }
+}
