8b72d343b84f1b83791b41cac6d894ab4eadad44
Ericpace644 <90404067+Ericpace644@users.noreply.github.com>
2023-03-21 13:40:53 -0700

    bc whdgefj

--- TBKR/Assets/Scripts/Backgroundcontroller.cs
+++ TBKR/Assets/Scripts/Backgroundcontroller.cs
@@ -5,17 +5,53 @@ using UnityEngine;
 public class Backgroundcontroller : MonoBehaviour
 {
 
-	public GameObject Background;
-	public GameObject Foreground;
-    
+    Transform player;
+
+    Vector3 tempPos;
+
+    [SerializeField]
+    float yLimit = 1f;
+
+    [SerializeField]
+    float xLimit = 1f;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").transform;
     }
 
-    
-    void FixedUpdate()
+    // Update is called once per frame
+    void LateUpdate()
     {
-        
+        tempPos = transform.position;
+        if (player != null)
+        {
+            if (Mathf.Abs(tempPos.x - player.position.x) > xLimit)
+            {
+                if (tempPos.x - player.position.x < 0)
+                {
+                    tempPos.x = player.position.x - xLimit;
+                }
+                else
+                {
+                    tempPos.x = player.position.x + xLimit;
+                }
+            }
+            if (Mathf.Abs(tempPos.y - player.position.y) > yLimit)
+            {
+                if (tempPos.y - player.position.y < 0)
+                {
+                    tempPos.y = player.position.y - yLimit;
+                }
+                else
+                {
+                    tempPos.y = player.position.y + yLimit;
+                }
+            }
+        }
+
+        transform.position = tempPos;
     }
+}
 }
